6c48974e73a955544a651cea24be5ec1ba7d039a
Abdul Rahman <abdulrahman161004@gmail.com>
2025-05-25 14:14:54 +0530

    feat: add creative stories dataset with images and links

---
+++ src/data.jsx
@@ -0,0 +1,76 @@
+const stories = [
+    {
+        profileImg: "./assets/profile-1.png",
+        profileName: "Behance",
+        title: [
+            "Showcasing creative",
+            "Portfolios and projects",
+            "form top designers",
+        ],
+        linkLabel: "Read More",
+        linkSrc: "behance.net",
+        storyImg: "./assets/story-1.avif",
+    },
+    {
+        profileImg: "./assets/profile-2.jpg",
+        profileName: "Behance",
+        title: [
+            "Showcasing creative",
+            "Portfolios and projects",
+            "form top designers",
+        ],
+        linkLabel: "Read More",
+        linkSrc: "behance.net",
+        storyImg: "./assets/story-2.jpg",
+    },
+    {
+        profileImg: "./assets/profile-3.jpg",
+        profileName: "Behance",
+        title: [
+            "Showcasing creative",
+            "Portfolios and projects",
+            "form top designers",
+        ],
+        linkLabel: "Read More",
+        linkSrc: "behance.net",
+        storyImg: "./assets/story-3.jpg",
+    },
+    {
+        profileImg: "./assets/profile-4.webp",
+        profileName: "Behance",
+        title: [
+            "Showcasing creative",
+            "Portfolios and projects",
+            "form top designers",
+        ],
+        linkLabel: "Read More",
+        linkSrc: "behance.net",
+        storyImg: "./assets/story-4.jpg",
+    },
+    {
+        profileImg: "./assets/profile-5.webp",
+        profileName: "Behance",
+        title: [
+            "Showcasing creative",
+            "Portfolios and projects",
+            "form top designers",
+        ],
+        linkLabel: "Read More",
+        linkSrc: "behance.net",
+        storyImg: "./assets/story-5.jpg",
+    },
+    {
+        profileImg: "./assets/profile-6.jpg",
+        profileName: "Behance",
+        title: [
+            "Showcasing creative",
+            "Portfolios and projects",
+            "form top designers",
+        ],
+        linkLabel: "Read More",
+        linkSrc: "behance.net",
+        storyImg: "./assets/story-6.jpg",
+    }
+
+]
+export default stories
